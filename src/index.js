cb06db9e43726c6f2d5821877289399dd8994b1a
yev <josephharveyangeles@gmail.com>
2018-10-06 16:02:30 +0800

    First working version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
+var ScrollAnimate = function(el, binding) {
+
+  var params = binding.value;
+  var modifiers = binding.modifiers;
+  var defaultClass = el.className;
+
+  function isInScrollView(rect){
+    return rect.top < document.documentElement.clientHeight && rect.bottom > 0;
+  }
+
+  return {
+    dispatch: function(isUpwards) {
+      if(!isInScrollView(el.getBoundingClientRect())) {
+        if (modifiers.repeat) { // there will be a decision matrix for isUpwards and params.up or params.down
+          // el.className = oldClasses;
+        }
+        return;
+      }
+
+      var classToApply;
+      if (typeof params === 'string') {
+        classToApply = params;
+      }
+      else if (params.down && params.up) { // implicit repeat
+        classToApply = isUpwards ? params.up : params.down;
+      } else {
+        classToApply = params.up || params.down;
+      }
+      el.className = defaultClass + ' ' + classToApply;
+    }
+  }
+}
+
 export default {
-  install(Vue, options) {
-    Vue.directive('vue-animate-onscroll', {
-    });
+  install(Vue, os = {}) {
+    var scrollAnimate;
+    Vue.directive('animate-onscroll', {
+      bind(el, binding) {
+        scrollAnimate = ScrollAnimate(el, binding);
+      },
+      inserted(el, binding) {
+        window.addEventListener('scroll', function() {
+          var isUpwards = this.oldScroll > this.scrollY;
+          scrollAnimate.dispatch(isUpwards);
+          this.oldScroll = this.scrollY;
+        });
+      }
+    })
   }
 }
