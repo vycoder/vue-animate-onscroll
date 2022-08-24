4ea2630c6378f32ffc2e3fd356b80200e664b8c4
Axel <develop4all@gmail.com>
2022-08-24 14:26:53 +0200

    Fix for repeating animations with absolute top or bottom positions.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ export default {
     Vue.directive('animate-onscroll', {
       inserted(el, binding) {
         const scrollAnimate = ScrollAnimate(Date.now())
-        const previousClassName = el.className
+        const params = binding.value;
+        const useParent = el.parentNode.childNodes.length == 1
         const offset = parseInt(el.dataset.animateOnscrollOffset || 0);
+        let previousClassName = el.className
         let lastScrollTop = window.pageYOffset
+        previousClassName = previousClassName.replace((params.in || params),'').replace(params.out,'');
         window.addEventListener('scroll', function() {
           let scrollTop = window.pageYOffset || document.documentElement.scrollTop
           const isUpwards = scrollTop < lastScrollTop
-          scrollAnimate.run(el, binding, {offset, isUpwards, previousClassName})
+          scrollAnimate.run(el, binding, {useParent, offset, isUpwards, previousClassName})
           lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
         }, false)
       }
